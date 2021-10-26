8ee695fe013ede780e34c3bf910ed9e67eb63374
NonT <NonT5498@gmail.com>
2021-10-25 23:00:21 -0700

    testing new crafting items
    added some metals to possibly consider the complication level of the crafting system hasnt been set in stone but i would like it to be something similar to this i have alot of other base items in mind for more variant in crafting

---
+++ gamemodes/newrp/schema/items/sh_common_metal.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Common Metals"
+ITEM.model = Model("models/gibs/metal_gib4.mdl")
+ITEM.description = "A Clump of simple and easy to gather metals, seems useful."
+ITEM.category = "Crafting"
+ITEM.width = 1
+ITEM.height = 1
---
+++ gamemodes/newrp/schema/items/sh_enchanted_metal.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Thaumaturgical Metals"
+ITEM.model = Model("models/gibs/metal_gib4.mdl")
+ITEM.description = "A jar full of gleaming metals, there covered in waving patterns giving off the impression of life."
+ITEM.category = "Crafting"
+ITEM.width = 1
+ITEM.height = 1
