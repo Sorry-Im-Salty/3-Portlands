d397c2cc89c03bbfb584c4643cfc447decc7b330
Lukaku <maggies00@hotmail.com>
2021-10-27 19:54:17 +1100

    Added shotgun

--- gamemodes/newrp/plugins/arccw_support/sh_arccw_weps.lua
+++ gamemodes/newrp/plugins/arccw_support/sh_arccw_weps.lua
@@ -55,3 +55,37 @@ PLUGIN.GunData[ "arccw_bocw_1911" ] = {
 	--Sec == Secondary
 	Sec = {}
 }
+
+PLUGIN.GunData[ "arccw_bocw_hauer77" ] = {
+	--Weapon can be Blacklisted and item will be not auto-generated
+	BlackList = false,
+	--Make sure exRender is always true for custom models, this keeps the inventory icons from fucking up
+	exRender = true,
+
+	Slot = "secondary",
+	Model = "models/weapons/arccw/w_bocw_hauer77.mdl",
+	iconCam = {
+		tpos = Vector( 0, 0, 0 ),
+		tang = Angle( 0, 0, 0 ),
+		tfov = 0
+	},
+	Width = 4,
+	Height = 1,
+	Weight = 3,
+	Price = 2000,
+	
+	--Weapon Parameters
+	--Prim == Primary
+	Prim = {
+		Ammo = "pistol",
+		Damage = 31,
+		KickUp = 0.7,
+		KickDown = 0.4,
+		KickHorizontal = 0.35,
+		Spread = .021,
+		IronAccuracy = .01
+	},
+	
+	--Sec == Secondary
+	Sec = {}
+}
