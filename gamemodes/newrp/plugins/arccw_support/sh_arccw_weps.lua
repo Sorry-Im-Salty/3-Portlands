3241ae84fa9616fece757974edb7f2efdf65d27f
Lukaku <maggies00@hotmail.com>
2021-10-28 19:53:37 +1100

    Added AR-C Removed 1911

--- gamemodes/newrp/plugins/arccw_support/sh_arccw_weps.lua
+++ gamemodes/newrp/plugins/arccw_support/sh_arccw_weps.lua
@@ -22,30 +22,30 @@
 
 
 
-PLUGIN.GunData[ "arccw_bocw_1911" ] = {
+PLUGIN.GunData[ "arccw_fml_blast_fc5_arc" ] = {
 	--Weapon can be Blacklisted and item will be not auto-generated
 	BlackList = false,
 	--Make sure exRender is always true for custom models, this keeps the inventory icons from fucking up
 	exRender = true,
 
-	Slot = "secondary",
-	Model = "models/weapons/arccw/w_cw_1911.mdl",
+	Slot = "primary",
+	Model = "models/weapons/arccw/fml/w_fatcry5_arc.mdl",
 	iconCam = {
 		tpos = Vector( 0, 0, 0 ),
 		tang = Angle( 0, 0, 0 ),
 		tfov = 0
 	},
 	Width = 2,
-	Height = 2,
+	Height = 3,
 	Weight = 3,
 	Price = 2000,
 	
 	--Weapon Parameters
 	--Prim == Primary
 	Prim = {
-		Ammo = "pistol",
-		Damage = 31,
-		KickUp = 0.7,
+		Ammo = "assault",
+		Damage = 25,
+		KickUp = 0.3,
 		KickDown = 0.4,
 		KickHorizontal = 0.35,
 		Spread = .021,
