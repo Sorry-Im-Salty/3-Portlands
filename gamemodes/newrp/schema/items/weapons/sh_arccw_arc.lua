0fc8ce728ce776d7f195320bbaff7feaf84b0ec4
Lukaku <maggies00@hotmail.com>
2021-10-29 17:23:18 +1100

    Removed old TFA base support files, re-created AR-C.

---
+++ gamemodes/newrp/schema/items/weapons/sh_arccw_arc.lua
@@ -0,0 +1,12 @@
+ITEM.name = "AR-C"
+ITEM.description = "A sidearm utilising .357 Caliber ammunition."
+ITEM.model = "models/weapons/arccw/fml/w_fatcry5_arc.mdl"
+ITEM.class = "arccw_fml_blast_fc5_arc"
+ITEM.weaponCategory = "primary"
+ITEM.width = 3
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(0, 0, 0),
+	fov	= 0,
+	pos	= Vector(0, 0, 0)
+}
