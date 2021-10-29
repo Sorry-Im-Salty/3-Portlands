e0382499152bc6a323ba36f53d0b300a051c18d9
Lukaku <maggies00@hotmail.com>
2021-10-29 19:02:13 +1100

    Added Assault Ammo Item

---
+++ gamemodes/newrp/schema/items/ammo/sh_assaultammo.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Assault Rifle Ammo"
+ITEM.model = "models/items/combine_rifle_cartridge01.mdl"
+ITEM.ammo = "smg1" -- type of the ammo
+ITEM.ammoAmount = 30 -- amount of the ammo
+ITEM.description = "A Cartridge that contains %s of AR2 Ammo"
+ITEM.classes = {CLASS_EOW}
--- gamemodes/newrp/schema/items/weapons/sh_arccw_arc.lua
+++ gamemodes/newrp/schema/items/weapons/sh_arccw_arc.lua
@@ -5,8 +5,5 @@ ITEM.class = "arccw_fml_blast_fc5_arc"
 ITEM.weaponCategory = "primary"
 ITEM.width = 3
 ITEM.height = 2
-ITEM.iconCam = {
-	ang	= Angle(0, 0, 0),
-	fov	= 0,
-	pos	= Vector(0, 0, 0)
-}
+ITEM.exRender = true
+ITEM.Ammo = "smg1"
